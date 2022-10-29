4929540b00aef1e603f756533ebe435be426cadd
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-10-29 22:03:44 +0300

    поправил решение

--- task5.py
+++ task5.py
@@ -16,3 +16,23 @@ for i in range(2, mounth+1):
  
 print('Необходимо взять в долг', (round((total - salary*12), 2)), 'рублей')
 
+# Траты растут ежемесячно по три процента от предыдущей суммы месяца.
+# Поэтому реализация выглядит так
+
+salary, expenses = 10000, 12000
+i = 0
+months_expenses = 0
+money_needs = 0
+
+while i < 10:
+
+    if i == 0:
+        months_expenses = expenses
+    elif i >= 1:
+        months_expenses *= 1.03
+    i += 1
+    difference = round(months_expenses-salary, 2)
+    money_needs += difference
+    print('Расходы в', i, 'месяце:', difference, ', Итого за', i, 'мес.:', round(money_needs,2))
+
+print('Сотруднику необходимо взять', money_needs, 'рублей')
