d0b3bc271717b077995d3c4aeeb49ed64756b1b3
Nikita Pyshkov <85805562+pyshkovni@users.noreply.github.com>
2022-10-29 21:53:39 +0300

    Выполнено

--- task3.py
+++ task3.py
@@ -14,3 +14,26 @@ elif month < 13:
     print('Число дней в этом месяце -', x[1])
 
 else: print('Некорректный номер месяца')
+
+# Ого! Интересный списочек!
+# Все ок. Можно и так
+
+# Решение 1
+if month in [1, 3, 5, 7, 8, 10, 12]:
+    print(31)
+elif month in [4, 6, 9, 11]:
+    print(30)
+elif month == 2:
+    print(28)
+else:
+    print('Введен неправильный номер месяца!')
+
+# Решение 2
+import calendar as cl  # используем модуль для получения функции
+
+year_input = input("Введите год: ")
+month_input = input("Введите номер месяца: ")
+
+year = int(year_input)
+month_ = int(month_input)
+print(cl.monthrange(year, month_))
